89db8c9197bbbeb7ee30ec9d4840d7869bb2071b
naveen211203 <100503680+naveen211203@users.noreply.github.com>
2022-12-12 19:20:34 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -247,7 +247,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://youtube.com/c/GreyMattersBot') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://youtube.com/@telugumoviedost7979') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
