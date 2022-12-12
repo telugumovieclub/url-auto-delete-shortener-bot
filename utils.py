1a3a945354ad61fb9906bcf3516c55214267e2ec
naveen211203 <100503680+naveen211203@users.noreply.github.com>
2022-12-12 21:25:49 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -383,7 +383,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://api.shareus.in/shortLink'
+    url = f'https://Mdisklink.link/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
               }
